4c9b307b1c1ffe9bda6307c59cd53cbb2a7a400c
Iñaki García Blasco <txorua@txorua.com>
2015-02-20 01:13:35 +0100

    Toco el slider

--- templates/views-view-list--slideshow-full--block.tpl.php
+++ templates/views-view-list--slideshow-full--block.tpl.php
@@ -1,4 +1,4 @@
-<div id="slideshow" class="fullwidthbanner-container main-slider hidden-xs">
+<div id="slideshow" class="fullwidthbanner-container main-slider">
   <div class="fullwidthbanner">
     <ul>
     <?php $rs_effect='fade'; ?>
@@ -19,17 +19,30 @@
         $teaser = ($field_slideshow_teaser) ? $field_slideshow_teaser[0]['safe_value'] : false;
       ?>
             
-      <li <?php if ($path): ?>data-link="<?php print url($path); ?>"<?php endif; ?> data-transition="<?php print $rs_effect ?>" data-masterspeed="800">
+      <li <?php if ($path): ?>data-link="<?php print url($path); ?>"<?php endif; ?> data-transition="<?php print $rs_effect ?>" data-slotamount="1" data-masterspeed="400" data-delay="5000">
 
-        <img data-bgposition="center top" src="<?php print $image; ?>"/>
+        <img src="<?php print $image; ?>"/>
 
-        <div class="tp-caption title-teaser sft fadeout"
-             data-x="left"
-             data-y="bottom"
-             data-speed="500"
-             data-start="1200"
-             data-voffset="-30"
-             data-easing="Power0.easeIn">
+        <div class="tp-caption title-teaser customin customout"
+            data-x="center"
+            data-y="center"
+
+            data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:18;scaleY:18;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+            data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+
+            data-start="1000"
+            data-speed="600"
+            data-easing="Linear.easeNone"
+
+            data-splitin="none"
+            data-elementdelay="0.1"
+
+            data-end="4500"
+            data-endspeed="600"
+            data-endeasing="Linear.easeNone"
+
+            data-splitout="none"
+            data-endelementdelay="0.1">
 
           <div class="intro-lead-in">
           <?php if ($path): ?>
@@ -37,12 +50,12 @@
           <?php else: ?>
             <?php print $title; ?>
           <?php endif; ?>
+          </div>
         </div>
 
           <?php if ($teaser): ?>
-          <p><?php print $teaser; ?></p>
+            <?php print $teaser; ?>
           <?php endif; ?>
-        </div>
 
         <div class="tp-caption caption-link sft fadeout"
              data-x="right"
@@ -58,7 +71,6 @@
               print l($text, url($path), array('attributes' => array('class' => array('more')), 'html' => TRUE)); ?>
           <?php endif; ?>
         </div>
-
       </li>
 
     <?php endforeach; ?>
@@ -87,7 +99,7 @@ drupal_add_js('
         delay:"'.$rs_effect_time.'",
         startheight:500,
         startwidth: 1140,
-        onHoverStop: "off",
+        onHoverStop: "on",
         shadow: 1
     });
     
