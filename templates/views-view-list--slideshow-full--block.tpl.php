354d37a8ac7544e3d61655368f389f1c8c056a4e
Iñaki García Blasco <txorua@txorua.com>
2015-02-26 18:18:59 +0100

    Reduzcoa el tiempo de aparición del segundo texto en el slide.

--- templates/views-view-list--slideshow-full--block.tpl.php
+++ templates/views-view-list--slideshow-full--block.tpl.php
@@ -41,7 +41,7 @@
 				  	 data-y="bottom" daata-voffset="-200"
   					 data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
 	  				 data-speed="500"
-		  			 data-start="1900"
+		  			 data-start="1000"
 			  		 data-easing="Back.easeOut"
 				  	 data-endspeed="500"
 					   data-endeasing="Power4.easeIn"
