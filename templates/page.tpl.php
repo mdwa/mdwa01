acd3d066ffbbf43e912dc01d8acd5e296fdd2190
Iñaki García Blasco <txorua@txorua.com>
2015-04-16 16:57:03 +0200

    Añado active-trail y quito

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -97,16 +97,6 @@
         <?php if ($page['sidebar_second']): ?>
           <div id="sidebar_second" class="col-sm-12 col-md-3">
             <?php print render($page['sidebar_second']); ?>
-
-            <?php if ($feed_icons): ?>
-            <div class="block">
-              <h2>RSS</h2>
-              <div class="content">
-                <p><?php print t('Subscribe to') . ' ' . $title . ' ' . $feed_icons; ?>
-              </div>
-            </div>
-            <?php endif; ?>
-
           </div>
         <?php endif; ?>
 
